a538a4d3659ad6e4949c211da00ea6c20eaa52cb
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-03-03 14:33:36 +0800

    Add refactored solution

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const toggleHidden = function () {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
+
+const activatePage = function () {
+  btnOpenModal.forEach(button =>
+    button.addEventListener('click', toggleHidden)
+  );
+
+  btnCloseModal.addEventListener('click', toggleHidden);
+  overlay.addEventListener('click', toggleHidden);
+
+  document.addEventListener('keydown', event => {
+    if (event.key === 'Escape' && !modal.classList.contains('hidden'))
+      toggleHidden();
+  });
+};
+
+window.addEventListener('load', activatePage);
