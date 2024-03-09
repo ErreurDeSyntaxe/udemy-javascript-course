ad81d22804e76252a2d39cb5bddc195f556ef94a
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-03-09 11:18:31 +0800

    Notes on destructuring

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,50 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2, 3, 4];
+// ES5 way of taking elements out of an array
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// ES6 destructuring
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(typeof x, typeof y, typeof z);
+
+// destructuring can skip elements in the array as show by 'premier' & 'troisieme'
+// const [first, second] = restaurant.categories;
+// console.log(first, second);
+// const [premier, , troisieme] = restaurant.categories;
+// console.log(premier, troisieme);
+
+let [main, secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// ES5 way of switching variables
+// const temp = main;
+// main = secondary;
+// secondary = main;
+// console.log(main, secondary);
+
+// SE6 way of switching variables: destructuring
+[main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// console.log(restaurant.order(2, 0));
+const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// Nested destructuring
+const nestedArray = [2, 4, [5, 6]];
+const [, , wantedArray] = nestedArray;
+// console.log(wantedArray);
+
+// destructuring within destructuring
+const [firstValue, , [secondValue, thirdValue]] = nestedArray;
+// console.log(firstValue, secondValue, thirdValue);
+
+// Default values: if there's no value, the variables are set to 1
+const [p = 1, q = 1, r = 1] = [420, 917];
+// console.log(p, q, r);
