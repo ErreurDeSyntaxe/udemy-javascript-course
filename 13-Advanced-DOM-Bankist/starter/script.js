c516f1851dca73879e0f0d8b28e00698113192d1
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-06-18 16:24:04 +0800

    Reveal section on scroll

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -107,6 +107,29 @@ const activateStickyNavBetter = function () {
 };
 activateStickyNavBetter();
 
+const revealOnScroll = function () {
+  const allSections = document.querySelectorAll('.section');
+  const revealSection = function (entries, observer) {
+    const [entry] = entries;
+    log(entry);
+
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  };
+
+  const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15,
+  });
+
+  allSections.forEach(function (section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+  });
+};
+revealOnScroll();
+
 /*
  *
  * Tab Component: Finding the elements and applying/removing styles
