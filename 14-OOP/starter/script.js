efdae805a40d5ffcbd7e5aa19d3025c0af537f39
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-06-22 11:23:13 +0800

    Learn about prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,6 +1,14 @@
 'use strict';
 const log = console.log;
 
+/*
+ *
+ *
+ * Constructors: Functions that return objects
+ *
+ *
+ */
+
 const learnAboutConstructors = function () {
   const Person = function (firstName, birthYear) {
     // this = {}
@@ -32,4 +40,48 @@ const learnAboutConstructors = function () {
   log('xavier instanceof Person:', xavier instanceof Person);
   log('johanne instanceof Person:', johanne instanceof Person);
 };
-learnAboutConstructors();
+// learnAboutConstructors();
+
+/*
+ *
+ *
+ * Prototypes: Blueprints of objects
+ *
+ *
+ */
+const learnAboutPrototypes = function () {
+  // Constructor
+  const Person = function (firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  };
+
+  // Add methods only on prototypes to benefit from inheritance
+  Person.prototype.calcAge = function () {
+    log(`${this.firstName} is ${2024 - this.birthYear} years old`);
+  };
+
+  const xavier = new Person('Xavier', 1988);
+  const jeanne = new Person('Jeanne', 1990);
+  xavier.calcAge(); // has access through prototypal inheritance
+  jeanne.calcAge(); // same
+
+  log(
+    'xavier.__proto__ === Person.prototype:',
+    xavier.__proto__ === Person.prototype
+  );
+  log(
+    'Person.prototype.isPrototypeOf(xavier):',
+    Person.prototype.isPrototypeOf(xavier)
+  );
+  log(
+    'Person.prototype.isPrototypeOf(Person):',
+    Person.prototype.isPrototypeOf(Person)
+  );
+
+  Person.prototype.species = 'Homo Sapiens';
+  log(xavier.species);
+  log('xavier.hasOwnProperty("firstName"):', xavier.hasOwnProperty('firstName'));
+  log('xavier.hasOwnProperty("species"):', xavier.hasOwnProperty('species'));
+};
+learnAboutPrototypes();
