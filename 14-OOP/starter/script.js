09c3e10b048c02ed5f5c30bc0da63f63caa95bfe
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-06-22 10:47:33 +0800

    Learn about constructors

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+const log = console.log;
+
+const learnAboutConstructors = function () {
+  const Person = function (firstName, birthYear) {
+    // this = {}
+
+    // instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // instance methods BAD PRACTICE!
+    this.calcAge = function () {
+      log(2024 - this.birthYear);
+    };
+    // instance methods BAD PRACTICE!
+    // instead, the methods should be on the prototype
+
+    // this = { firstName, birthYear }
+  };
+
+  // 1. New {} (empty object) is created
+  // 2. function is called, this = {}
+  // 3. {} is linked to prototype
+  // 4. constructor function returns {}
+  const xavier = new Person('Xavier', 1988);
+  const jeanne = new Person('Jeanne', 1990);
+  const maite = new Person('Maïté', 2020);
+  const johanne = { firstName: 'Johanne', birthYear: 1961 };
+
+  log(johanne, xavier, jeanne, maite);
+  log('xavier instanceof Person:', xavier instanceof Person);
+  log('johanne instanceof Person:', johanne instanceof Person);
+};
+learnAboutConstructors();
