929985f33f04fcca52a648eadd59a533f46ae644
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-06-23 08:55:14 +0800

    Learn about inheritance

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -4,7 +4,7 @@ const log = console.log;
 /*
  *
  *
- * Constructors: Functions that return objects
+ * Constructors: Object Return Functions
  *
  *
  */
@@ -45,7 +45,7 @@ const learnAboutConstructors = function () {
 /*
  *
  *
- * Prototypes: Blueprints of objects
+ * Prototypes: Object Blueprints
  *
  *
  */
@@ -69,19 +69,59 @@ const learnAboutPrototypes = function () {
   log(
     'xavier.__proto__ === Person.prototype:',
     xavier.__proto__ === Person.prototype
-  );
+  ); // true
   log(
     'Person.prototype.isPrototypeOf(xavier):',
     Person.prototype.isPrototypeOf(xavier)
-  );
+  ); // true
   log(
     'Person.prototype.isPrototypeOf(Person):',
     Person.prototype.isPrototypeOf(Person)
-  );
+  ); // false
 
   Person.prototype.species = 'Homo Sapiens';
-  log(xavier.species);
-  log('xavier.hasOwnProperty("firstName"):', xavier.hasOwnProperty('firstName'));
-  log('xavier.hasOwnProperty("species"):', xavier.hasOwnProperty('species'));
+  log(xavier.species); // accessible thru prototype, not the object itself
+  log(
+    'xavier.hasOwnProperty("firstName"):',
+    xavier.hasOwnProperty('firstName')
+  ); // true
+  log('xavier.hasOwnProperty("species"):', xavier.hasOwnProperty('species')); // false
 };
-learnAboutPrototypes();
+// learnAboutPrototypes();
+
+const learnAboutInheritance = function () {
+  const Person = function (firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  };
+
+  Person.prototype.calcAge = function () {
+    this.age = 2024 - this.birthYear;
+    return this.age;
+  };
+
+  const xavier = new Person('Xavier', 1988);
+
+  // prototype of all Person {}
+  log('xavier.__proto__:', xavier.__proto__);
+  // prototype of the Person constructor
+  log('xavier.__proto__^2:', xavier.__proto__.__proto__);
+  // null
+  log('xavier.__proto__^3:', xavier.__proto__.__proto__.__proto__);
+  // Person
+  console.dir(Person.prototype.constructor);
+
+  const arr = [2, 4, 2, 1, 3, 2, 1, 3, 4];
+  log(arr.__proto__);
+  log(arr);
+
+  Array.prototype.unique = function () {
+    return [...new Set(this)];
+  };
+  log(arr.unique()); // Not a great idea of modifying a built-in Objects
+
+  const h1 = document.querySelector('h1');
+  console.dir(h1);
+  console.dir(log);
+};
+learnAboutInheritance();
