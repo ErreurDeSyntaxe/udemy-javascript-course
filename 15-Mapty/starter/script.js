2be7b43bca9fe04c5bbedb71501068aba7cc31ef
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-07-12 05:52:25 -0400

    Learn basics of geolocation

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,32 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+/*
+ *
+ *
+ * Geolocation API: Very basics of the API
+ *
+ *
+ */
+const learnAboutGeolocation = function () {
+  if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        console.log(position);
+        console.log(latitude, longitude);
+        console.log(
+          'https://www.google.com/maps/@45.5578342,-73.532523,14z?entry=ttu'
+        );
+        console.log(
+          `https://www.google.com/maps/@${latitude},${longitude},14z?entry=ttu`
+        );
+      },
+      function () {
+        alert('Could not get your position');
+      }
+    );
+};
+learnAboutGeolocation();
