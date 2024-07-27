67eb2d5a1261b8db27db212bc6acec40b4a3bdd6
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-07-27 06:41:10 -0400

    Add workout classes

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -19,6 +19,63 @@ const inputElevation = document.querySelector('.form__input--elevation');
  *
  */
 
+/*
+ * Workout: The parent class
+ */
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+/*
+ * Running: Child class
+ */
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // minutes/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+/*
+ * Cycling: Child class
+ */
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+// setTimeout so the IDs are different
+setTimeout(() => {
+  const bike1 = new Cycling([39, -12], 27, 95, 523);
+  console.log(run1, bike1);
+}, 10);
+
+/*
+ * The Application: The map, the events (click & submit)
+ */
 class App {
   #map;
   #mapEvent;
