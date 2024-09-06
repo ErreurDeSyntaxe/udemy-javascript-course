31d16ddbfb95878433d12065168e176e8e0ccfa0
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-09-06 11:28:08 +0800

    Take notes on sync vs async

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,4 +3,31 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+/*
+ *
+ *
+ * Learning Section: Asynchronous JS, Promises, Async/Await, AJAX, APIs
+ *
+ *
+ */
+
+// course update: the API changed its URL
+// https://countries-api-836d.onrender.com/countries/
+
+/*
+ * Synchronous vs Asynchronous
+ */
+const learnAboutSynchronous = function () {
+  const para = document.querySelector('.p'); // synchronous
+  setTimeout(() => (para.style.color = 'red'), 2000); // defered
+  para.textContent = 'Xavier'; // sync
+  // the color is set to 'red' way after the content it set to 'Xavier'
+  // as opposed to immediately set to red
+
+  const img = document.createElement('img');
+  img.src = '';
+  // this action is asynchronous although it looks synchronous
+  img.addEventListener('load', () => (img.style.height = '200px'));
+  // this action is synchronous. event listeners are NOT async
+};
+// learnAboutSynchronous();
