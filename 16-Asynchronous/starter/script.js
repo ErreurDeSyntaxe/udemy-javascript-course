ef454b295c8293049b1256f34f10fc33ff97ba87
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-09-19 16:19:26 +0800

    Learn Promise.all()

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -734,4 +734,47 @@ const learnAboutAsynchrony = function () {
     console.log('3: This prints third');
   })();
 };
-learnAboutAsynchrony();
+// learnAboutAsynchrony();
+
+/*
+ * Practice Async/Await
+ */
+const parallelRequests = function () {
+  const get3CountriesV1 = async function (c1, c2, c3) {
+    try {
+      const [data1] = await getJSON(
+        `https://restcountries.com/v3.1/name/${c1}`
+      );
+      const [data2] = await getJSON(
+        `https://restcountries.com/v3.1/name/${c2}`
+      );
+      const [data3] = await getJSON(
+        `https://restcountries.com/v3.1/name/${c3}`
+      );
+
+      console.log(...data1.capital, ...data2.capital, ...data3.capital);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  // it makes no sense for the 2nd to wait for the 1st
+  // they should be handled in parallel
+  // get3CountriesV1('Portugal', 'Canada', 'Tanzania');
+
+  const get3CountriesV2 = async function (c1, c2, c3) {
+    try {
+      const data = await Promise.all([
+        getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+        getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+        getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+      ]);
+
+      const capitals = data.map(d => d[0].capital[0]);
+      console.log(...capitals);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  get3CountriesV2('Portugal', 'Canada', 'Tanzania');
+};
+parallelRequests();
