f145134685c814f03a5425073a768b951db06aac
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-09-20 08:51:01 +0800

    Do challenge 3

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -777,4 +777,77 @@ const parallelRequests = function () {
   };
   get3CountriesV2('Portugal', 'Canada', 'Tanzania');
 };
-parallelRequests();
+// parallelRequests();
+
+/*
+ * Coding Challenge 3
+ */
+const challenge3 = function () {
+  console.log('-Coding Challenge 3-');
+
+  btn.style.display = 'none';
+  const imgContainer = document.querySelector('.images');
+
+  const wait = function (seconds) {
+    return new Promise(function (resolve) {
+      setTimeout(resolve, seconds * 1000);
+    });
+  };
+
+  const createImage = function (imgPath) {
+    return new Promise(function (resolve, reject) {
+      const img = document.createElement('img');
+      img.src = imgPath;
+
+      img.addEventListener('load', function () {
+        imgContainer.append(img);
+        resolve(img);
+      });
+
+      img.addEventListener('error', function () {
+        reject(new Error('Image not found'));
+      });
+    });
+  };
+
+  const loadNpause = async function () {
+    try {
+      let img = await createImage('./img/img-1.jpg');
+      await wait(2);
+      img.style.display = 'none';
+      img = await createImage('./img/img-2.jpg');
+      await wait(2);
+      img.style.display = 'none';
+      img = await createImage('./img/img-3.jpg');
+      await wait(2);
+      img.style.display = 'none';
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  // loadNpause();
+
+  const loadAll = async function (imgArr) {
+    try {
+      const imgs = imgArr.map(async img => await createImage(img));
+      console.log(imgs);
+      const imgsEl = await Promise.all(imgs);
+      console.log(imgsEl);
+
+      imgsEl.forEach(i => i.classList.add('parallel'));
+    } catch (err) {
+      console.error(err);
+    }
+
+    // console.log(imgArr);
+    // const imgs = imgArr.map(i => createImage(i));
+    // console.log(imgs);
+    // const promises = await Promise.all([imgs[0], imgs[1], imgs[2]]);
+    // console.log(promises);
+    // promises[0].classList.add('parallel');
+    // promises[1].classList.add('parallel');
+    // promises[2].classList.add('parallel');
+  };
+  loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
+};
+// challenge3();
